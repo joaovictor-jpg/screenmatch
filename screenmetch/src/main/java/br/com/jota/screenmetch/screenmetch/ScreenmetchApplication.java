d33a886d6e517fca6375f56eff61f261f232a78c
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2024-12-17 20:59:28 -0300

    :tada: Commit inicial

---
+++ screenmetch/src/main/java/br/com/jota/screenmetch/screenmetch/ScreenmetchApplication.java
@@ -0,0 +1,13 @@
+package br.com.jota.screenmetch.screenmetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmetchApplication.class, args);
+	}
+
+}
---
+++ screenmetch/src/test/java/br/com/jota/screenmetch/screenmetch/ScreenmetchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jota.screenmetch.screenmetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
