7538a3454d79c2aafbcb2884957dca24802417c2
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2024-12-17 21:59:50 -0300

    :wrench: feat: configuração do docker-compose

--- screenmetch/src/main/java/br/com/jota/screenmetch/screenmetch/ScreenmetchApplication.java
+++ screenmetch/src/main/java/br/com/jota/screenmetch/screenmetch/ScreenmetchApplication.java
@@ -1,13 +1,17 @@
 package br.com.jota.screenmetch.screenmetch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmetchApplication {
+public class ScreenmetchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmetchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
