e0834a13492b7e86c789f0004961a619235a46a9
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2024-12-19 22:24:11 -0300

    :sparkles: feat: Lista séries, buscar série por titulo, buscar série por ator, top 5 séries e buscar por categória

--- screenmetch/src/main/java/br/com/jota/screenmetch/screenmetch/principal/Principal.java
+++ screenmetch/src/main/java/br/com/jota/screenmetch/screenmetch/principal/Principal.java
@@ -2,11 +2,14 @@ package br.com.jota.screenmetch.screenmetch.principal;
 
 import br.com.jota.screenmetch.screenmetch.models.Serie;
 import br.com.jota.screenmetch.screenmetch.models.dto.DadosSerie;
+import br.com.jota.screenmetch.screenmetch.models.enums.Categoria;
 import br.com.jota.screenmetch.screenmetch.repository.SerieRepository;
 import br.com.jota.screenmetch.screenmetch.services.ConsultaGemini;
 import br.com.jota.screenmetch.screenmetch.services.ConverteDados;
 import br.com.jota.screenmetch.screenmetch.services.ObterDadosServices;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -17,6 +20,7 @@ public class Principal {
     private ConverteDados converteDados = new ConverteDados();
     private String token;
     private SerieRepository serieRepository;
+    private List<Serie> serie;
 
     public Principal(String apiKey, String token, SerieRepository serieRepository) {
         this.apiKey = apiKey;
@@ -33,6 +37,11 @@ public class Principal {
         while (opcoes != 0) {
             var menu = """
                         1 - Buscar série
+                        2 - Lista série
+                        3 - Buscar série por título
+                        4 - Buscar série por autor
+                        5 - Top 5 fovoritas séries
+                        6 - Buscar Por categória
                         0 - Sair
                     """;
 
@@ -44,6 +53,21 @@ public class Principal {
                 case 1:
                     buscarSerieWeb();
                     break;
+                case 2:
+                    listaSerieBuscadas();
+                    break;
+                case 3:
+                    buscarSeriePorTittulo();
+                    break;
+                case 4:
+                    buscarSeriePorAutor();
+                    break;
+                case 5:
+                    top5FavoritasSeries();
+                    break;
+                case 6:
+                    buscarSeriePorCategoria();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -52,6 +76,46 @@ public class Principal {
             }
         }
     }
+
+    private void buscarSeriePorCategoria() {
+        System.out.println("Digite a categoria");
+        var categoria = scanner.nextLine();
+        Categoria categoria1 = Categoria.fromStringPtBr(categoria);
+        var series = serieRepository.findByGenero(categoria1);
+        series.stream().sorted(Comparator.comparing(Serie::getGenero)).forEach(System.out::println);
+    }
+
+    private void top5FavoritasSeries() {
+        List<Serie> series = serieRepository.findTop5ByOrderByAvaliacaoDesc();
+        series.stream()
+                .forEach(System.out::println);
+    }
+
+    private void buscarSeriePorAutor() {
+        System.out.println("Digite o nome do ator");
+        var nomeAtor = scanner.nextLine();
+        System.out.println("Digite a Nota de avaliação");
+        var nota = scanner.nextDouble();
+        List<Serie> serie = serieRepository.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, nota);
+        serie.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTittulo() {
+        System.out.println("Entre com título da serie");
+        var titulo = scanner.nextLine();
+        Serie serie = serieRepository.findByTituloContainingIgnoreCase(titulo);
+        System.out.println(serie);
+    }
+
+    private void listaSerieBuscadas() {
+        this.serie = serieRepository.findAll();
+        serie.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+    }
+
 
     private DadosSerie getDadosSerie() {
         System.out.println("Digite o nome da série para busca");
@@ -65,6 +129,7 @@ public class Principal {
         DadosSerie dados = getDadosSerie();
         var sinopse = ConsultaGemini.obterTraducao(dados.sinopse().trim(), this.token);
         serieRepository.save(new Serie(dados, sinopse));
+        System.out.println(dados);
     }
 
 }
--- screenmetch/src/main/java/br/com/jota/screenmetch/screenmetch/repository/SerieRepository.java
+++ screenmetch/src/main/java/br/com/jota/screenmetch/screenmetch/repository/SerieRepository.java
@@ -1,7 +1,17 @@
 package br.com.jota.screenmetch.screenmetch.repository;
 
 import br.com.jota.screenmetch.screenmetch.models.Serie;
+import br.com.jota.screenmetch.screenmetch.models.enums.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Serie findByTituloContainingIgnoreCase(String titulo);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String atores, Double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria1);
 }
