fc6bb37a65f998c0946dd2d724b817d678b32c46
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2024-12-22 20:21:50 -0300

    Finalizando primeira parte do front

---
+++ webscreematch/scripts/getDados.js
@@ -0,0 +1,9 @@
+const baseURL = 'http://localhost:8080';
+
+export default function getDados(endPoint) {
+    return fetch(`${baseURL}${endPoint}`)
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Erro ao acessar o endpoint /series/top5:', error);
+        })
+}
